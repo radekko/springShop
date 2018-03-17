80f0d45ff34bae4260810d6c2322851728b92ca0
Bartek <Bartek@radek>
2018-03-17 10:21:11 +0100

    Register user test

---
+++ SpringShop/src/test/java/test/springshop/CartControllerTest.java
@@ -0,0 +1,67 @@
+package test.springshop;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+import com.shop.config.RootConfig;
+import com.shop.model.LineItem;
+import com.shop.service.CartService;
+
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes=RootConfig.class)
+@WebAppConfiguration
+public class CartControllerTest {
+
+	@Autowired
+	CartService cartService;
+	
+	LineItem bookArray;
+	LineItem appleArray;
+	String username;
+	String BOOK_UNIQUE_CODE = "aa";
+	String APPLE_UNIQUE_CODE = "bb";
+	
+	@Before
+	public void initializeLineItems() {
+		 bookArray = new LineItem("book",BOOK_UNIQUE_CODE,5.0,2);
+		 appleArray = new LineItem("apple",APPLE_UNIQUE_CODE,25.0,3);
+		 username = "testUser";
+		 cartService.setUsername(username);
+	}
+	
+	@Test
+	public void testItemsAddintgToCartAreUnique() throws Exception{
+		cartService.addItem(bookArray);
+		cartService.addItem(bookArray);
+		assertEquals(1, getCartSize());
+	}
+	
+	@Test
+	public void testItemsRemovingFromCart() throws Exception{
+		cartService.addItem(bookArray);
+		cartService.addItem(appleArray);
+		assertEquals(2, cartService.getCart().size() );
+		cartService.removeItem(APPLE_UNIQUE_CODE);
+		assertEquals(1, getCartSize());
+	}
+	
+	@Test
+	public void testClearCart() throws Exception{
+		cartService.addItem(bookArray);
+		cartService.addItem(bookArray);
+		cartService.clearCart();
+		assertEquals(0, getCartSize());
+	}
+	
+	private int getCartSize() {
+		return cartService.getCart().size();
+	}
+}
