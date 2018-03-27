70eaec399a339216f193796f30e071187f42c05a
Bartek <Bartek@radek>
2018-03-27 11:46:30 +0200

    NavigationPagesCreator refactoring

--- SpringShop/src/main/java/com/shop/service/NavigationPagesCreator.java
+++ SpringShop/src/main/java/com/shop/service/NavigationPagesCreator.java
@@ -10,36 +10,55 @@ public class NavigationPagesCreator {
 		int cur = numberOfChosenPage;
 		int maxNav = maxNavigationPages;
 		int total = totalPages;
-		
-		
-		if(total <= maxNav) {
-			for(int i=1; i<=total; i++)
-				navigationPages.add(i);
-		}
-		else {
-			navigationPages.add(1);
-			
-			//1
-			if(cur<=2) {
-				for(int i=2; i<maxNav; i++){
-					navigationPages.add(i);
-				}
-			}
-			//2
-			else if(cur>2 && cur < total- ( maxNav - 2)) {
-				for(int i=cur; i<maxNav + (cur - 2) ; i++) {
-					navigationPages.add(i);
-				}
-			} //3
-			else {
-				for(int i= total - (maxNav - 2); i<total; i++) {
-					navigationPages.add(i);
-				}
-			}
-			navigationPages.add(totalPages);
-		}
+
+		if (total <= maxNav) 
+			p1234N(navigationPages, total);
+		 else {
+			addFirstNavPage(navigationPages);
+			addCentralNavPages(navigationPages, cur, maxNav, total);
+			addLastNavPage(navigationPages, total);
+		 }
 
 		return navigationPages;
 	}
-	
+
+	private static void addLastNavPage(List<Integer> navigationPages, int total) {
+		navigationPages.add(total);
+	}
+
+	private static void addFirstNavPage(List<Integer> navigationPages) {
+		navigationPages.add(1);
+	}
+
+	private static void addCentralNavPages(List<Integer> navigationPages, int cur,
+			int maxNav, int total) {
+
+		if (cur <= 2) 
+			p1234xN(navigationPages, maxNav);
+		else if (cur > 2 && cur < total - (maxNav - 2)) 
+			p1x345xN(navigationPages, cur, maxNav);
+		else 
+			p1x789N(navigationPages, maxNav, total);
+	}
+
+	private static void p1x789N(List<Integer> navigationPages, int maxNav, int total) {
+		for (int i = total - (maxNav - 2); i < total; i++)
+			navigationPages.add(i);
+	}
+
+	private static void p1x345xN(List<Integer> navigationPages, int cur, int maxNav) {
+		for (int i = cur; i < maxNav + (cur - 2); i++)
+			navigationPages.add(i);
+	}
+
+	private static void p1234xN(List<Integer> navigationPages, int maxNav) {
+		for (int i = 2; i < maxNav; i++)
+			navigationPages.add(i);
+	}
+
+	private static void p1234N(List<Integer> navigationPages, int total) {
+		for (int i = 1; i <= total; i++)
+			navigationPages.add(i);
+	}
+
 }
