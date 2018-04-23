6f75a379e983d5d169480cf72341be10bb7c5082
Bartek <Bartek@radek>
2018-04-23 09:15:29 +0200

    Add column name

--- SpringShop/src/main/java/com/shop/model/entity/persistent/Product.java
+++ SpringShop/src/main/java/com/shop/model/entity/persistent/Product.java
@@ -7,6 +7,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
@@ -25,6 +26,7 @@ public class Product {
 	private List<Order> orders;
 	
 	@ManyToOne(cascade = CascadeType.ALL)
+	@JoinColumn(name = "categoryId")
 	private Category category;
 	
 	public String getName() {
