ca2b84d3ce418481576a1be27447ac31629a75fd
Bartek <Bartek@radek>
2018-04-26 16:08:35 +0200

    Add lazy loading for category

--- SpringShop/src/main/java/com/shop/model/entity/persistent/Product.java
+++ SpringShop/src/main/java/com/shop/model/entity/persistent/Product.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -25,7 +26,7 @@ public class Product {
 	@ManyToMany(mappedBy="products")
 	private List<Order> orders;
 	
-	@ManyToOne(cascade = CascadeType.ALL)
+	@ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
 	@JoinColumn(name = "categoryId")
 	private Category category;
 	
