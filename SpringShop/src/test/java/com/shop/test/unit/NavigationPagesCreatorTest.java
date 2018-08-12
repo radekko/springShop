deaf97b290991669676481f601eefc60375021ad
radek <myemail@example.com>
2018-08-12 13:25:48 +0200

    refactor test

--- SpringShop/src/test/java/com/shop/test/unit/NavigationPagesCreatorTest.java
+++ SpringShop/src/test/java/com/shop/test/unit/NavigationPagesCreatorTest.java
@@ -6,8 +6,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-import org.hamcrest.Matcher;
-import org.hamcrest.Matchers;
+import static org.hamcrest.Matchers.equalTo;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -15,18 +14,18 @@ import com.shop.pagination.NavigationPagesCreator;
 
 public class NavigationPagesCreatorTest {
 
-	private List<Integer> list1;
-	private List<Integer> list2;
-	private List<Integer> list3;
+	private List<Integer> npFirstCase;
+	private List<Integer> npSecondCase;
+	private List<Integer> npThirdCase;
 	private int maxNavigationPages;
 	private int totalItems;
 	private int maxProductOnPage;
 	
     @Before
     public void setUp() {
-    	list1 = new ArrayList<Integer>(Arrays.asList(1,2,3,4,-1,7));
-    	list2 = new ArrayList<Integer>(Arrays.asList(1,-1,3,4,5,-1,7));
-    	list3 = new ArrayList<Integer>(Arrays.asList(1,-1,4,5,6,7));
+    	npFirstCase = new ArrayList<Integer>(Arrays.asList(1,2,3,4,-1,7));
+    	npSecondCase = new ArrayList<Integer>(Arrays.asList(1,-1,3,4,5,-1,7));
+    	npThirdCase = new ArrayList<Integer>(Arrays.asList(1,-1,4,5,6,7));
     	maxNavigationPages = 5;
     	totalItems=21;
     	maxProductOnPage=3;
@@ -34,20 +33,20 @@ public class NavigationPagesCreatorTest {
 	
 	@Test
 	public void testCreateNavigationPages() throws Exception{
-		assertThat(getNP(1),getResult(list1));
-		assertThat(getNP(2),getResult(list1));
-		assertThat(getNP(3),getResult(list1));
-		assertThat(getNP(4),getResult(list2));
-		assertThat(getNP(5),getResult(list3));
-		assertThat(getNP(6),getResult(list3));
-		assertThat(getNP(7),getResult(list3));
+		testIfCreatorReturnCorrectCaseForChosenPage(1,npFirstCase);
+		testIfCreatorReturnCorrectCaseForChosenPage(2,npFirstCase);
+		testIfCreatorReturnCorrectCaseForChosenPage(3,npFirstCase);
+		testIfCreatorReturnCorrectCaseForChosenPage(4,npSecondCase);
+		testIfCreatorReturnCorrectCaseForChosenPage(5,npThirdCase);
+		testIfCreatorReturnCorrectCaseForChosenPage(6,npThirdCase);
+		testIfCreatorReturnCorrectCaseForChosenPage(7,npThirdCase);
+	}
+	
+	private void testIfCreatorReturnCorrectCaseForChosenPage(int page, List<Integer> expectNpCase) {
+		assertThat(getNPForChosenPage(page),equalTo(expectNpCase));
 	}
 
-	private Matcher<List<Integer>> getResult(List<Integer> list) {
-		return Matchers.is(list);
-	}
-
-	private List<Integer> getNP(int currPage) {
+	private List<Integer> getNPForChosenPage(int currPage) {
 		return NavigationPagesCreator.createNavigationPages(currPage, maxNavigationPages, totalItems,maxProductOnPage);
 	}
 	
